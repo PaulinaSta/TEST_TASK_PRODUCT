c64f55af04b9358ad986c0acb5d553c74ee6ffff
Paulina <paulinaa.staszynska@gmail.com>
2021-09-04 19:27:53 +0200

    Creating section testimonials

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-new Glide('.glide', {
+new Glide('.testimonials__glide', {
+    type: 'carousel',
+    perView: 2,
+    gap: 30,
+}).mount()
+
+
+new Glide('.accessories__glide', {
     type: 'carousel',
     perView: 4,
     gap: 30,
