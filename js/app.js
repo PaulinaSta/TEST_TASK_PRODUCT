49c6153a2b2bddc7f0ee170fb9a9e8aa77e3a191
Paulina <paulinaa.staszynska@gmail.com>
2021-09-05 14:34:24 +0200

    Correcting glide js

--- js/app.js
+++ js/app.js
@@ -9,10 +9,3 @@ new Glide('.testimonials__glide', {
     perView: 2,
     gap: 30,
 }).mount()
-
-
-new Glide('.accessories__glide', {
-    type: 'carousel',
-    perView: 4,
-    gap: 30,
-}).mount()
